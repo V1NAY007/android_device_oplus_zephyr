373c1579616ac0a32e1d379e9d8823be2f42f833
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 06:33:38 +0300

    zephyr: Remove unused build description override
    Change-Id: I3d53f1cc5d4711403b53b723b95a175e6220702c

--- lineage_zephyr.mk
+++ lineage_zephyr.mk
@@ -8,21 +8,18 @@ $(call inherit-product, device/oplus/zephyr/device.mk)
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_BRAND := Oplus
-PRODUCT_DEVICE := zephyr
-PRODUCT_MANUFACTURER := Oplus
-PRODUCT_MODEL := mt6895
 PRODUCT_NAME := lineage_zephyr
+PRODUCT_DEVICE := zephyr
+PRODUCT_MANUFACTURER := OPlus
+PRODUCT_BRAND := OPlus
+PRODUCT_MODEL := mt6895
+PRODUCT_SYSTEM_NAME := zephyr
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="sys_mssi_64_cn_armv82-user 15 AP3A.240617.008 1755697740151 release-keys" \
     BuildFingerprint=OnePlus/PGKM10/OP5565:15/AP3A.240617.008/S.1f56c75-1-a469:user/release-keys \
-    DeviceName=PGKM10 \
-    DeviceProduct=PGKM10 \
-    SystemDevice=PGKM10 \
-    SystemName=PGKM10
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
 #Axion Stuff
 TARGET_ENABLE_BLUR := true
