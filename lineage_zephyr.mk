7c6e0635002c7a4f7eb8b3bbfc180f406c2ed638
V1NAY007 <shandilyavinay0@gmail.com>
2026-07-21 21:36:05 +0530

    Bringup Axion

--- lineage_zephyr.mk
+++ lineage_zephyr.mk
@@ -5,6 +5,7 @@
 $(call inherit-product, device/oplus/zephyr/device.mk)
 
 # Inherit from the LineageOS configuration.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Oplus
@@ -23,3 +24,23 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     SystemDevice=PGKM10 \
     SystemName=PGKM10
 
+#Axion Stuff
+TARGET_ENABLE_BLUR := true
+TARGET_INCLUDE_AXFX := true
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,8,2
+AXION_CAMERA_FRONT_INFO := 16
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := V1NAY007
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Dimensity_8100
+# CPU governor support
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := performance
+PERF_ANIM_OVERRIDE := true
+HBM_SUPPORTED := true
+HBM_NODE := /sys/class/backlight/panel0-backlight/hbm_mode
+TORCH_STR_SUPPORTED := true
+TARGET_INCLUDES_LOS_PREBUILTS := true
+TARGET_NEEDS_DOZE_FIX := true
+TARGET_SUPPORTED_REFRESH_RATES := 60,75,90,120
