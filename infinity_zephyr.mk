1d2e2a639a74cf74d8ed9587149cd36246d10631
V1NAY007 <shandilyavinay0@gmail.com>
2026-07-26 12:38:55 +0530

    zephyr: Bringup Infinityx

--- infinity_zephyr.mk
+++ infinity_zephyr.mk
@@ -5,9 +5,9 @@
 $(call inherit-product, device/oplus/zephyr/device.mk)
 
 # Inherit from the LineageOS configuration.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_zephyr
+PRODUCT_NAME := infinity_zephyr
 PRODUCT_DEVICE := zephyr
 PRODUCT_MANUFACTURER := OPlus
 PRODUCT_BRAND := OPlus
@@ -19,4 +19,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=OnePlus/PGKM10/OP5565:15/AP3A.240617.008/S.1f56c75-1-a469:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+# Infinity Stuff
+INFINITY_MAINTAINER := "V1NAY007"
+TARGET_HAS_UDFPS := true
+WITH_GAPPS := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_MOTO_CALCULATOR := true
 
