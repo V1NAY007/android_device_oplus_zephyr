9c622cde4b5e16b6e5efc44df409ef9abc87c9e9
V1NAY007 <shandilyavinay0@gmail.com>
2026-08-17 15:45:31 +0000

    zephyr: 4k60fps for Aperture

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,12 @@ cd vendor/lumine-priv/keys
 ./keys.sh
 cd ../../..
 
+echo "Unlock 4K 60FPS Cam Recording"
+cd p*/a*/Aperture
+git fetch https://github.com/Adarsh0127-Elite/android_packages_apps_Aperture.git
+git cherry-pick 9509277efc852ad8bdcce204e0d9cfe104b6d190
+cd ../../..
+
 echo "Clonning IMS"
 git clone -b android-16-qpr2 https://github.com/Adarsh0127-Elite/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
