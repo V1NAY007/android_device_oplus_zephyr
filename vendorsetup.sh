ebbf6d2cf02aee9375981da99a3889709ed14df1
V1NAY007 <shandilyavinay0@gmail.com>
2026-08-17 15:30:06 +0000

    zephyr: Use private keys

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Clonning Private keys"
+croot && git clone https://github.com/ProjectMatrixx/vendor_matrixx-priv_keys-template vendor/lumine-priv/keys
+cd vendor/lumine-priv/keys
+./keys.sh
+cd ../../..
+
 echo "Clonning IMS"
 git clone -b android-16-qpr2 https://github.com/Adarsh0127-Elite/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
