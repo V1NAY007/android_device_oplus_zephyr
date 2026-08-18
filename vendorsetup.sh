967ebcb4f08766ab03f264416b36684ac18e1ae4
V1NAY007 <shandilyavinay0@gmail.com>
2026-08-18 15:39:25 +0000

    vendorsetup: Unlock 4K60Fps in Aperture

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,4 +3,8 @@
 echo "Clonning IMS"
 git clone -b android-16-qpr2 https://github.com/Adarsh0127-Elite/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
-
+echo "Unlock 4K 60FPS Cam Recording"
+cd p*/a*/Aperture
+git fetch https://github.com/Adarsh0127-Elite/android_packages_apps_Aperture.git
+git cherry-pick 9509277efc852ad8bdcce204e0d9cfe104b6d190
+cd ../../..
